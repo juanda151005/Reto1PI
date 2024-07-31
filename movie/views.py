275f7ee670ff99e47a46301aaa4b9871c663b002
Juanda151005 <juandavidvr151005@gmail.com>
2024-07-31 11:19:03 -0500

    modificacion funcion about

--- movie/views.py
+++ movie/views.py
@@ -8,4 +8,5 @@ def home(request):
     return render(request,'home.html', {'name':'Juan Velasquez'})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page')
+    #return HttpResponse('<h1>Welcome to About Page')
+    return render(request, 'about.html')
